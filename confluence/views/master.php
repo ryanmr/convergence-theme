fd4b93892b37c5bae5d5493849a4042c6c2aa1b0
Ryan Rampersad <ryan.rampersad@gmail.com>
2012-11-17 00:24:41 -0600

    Modified master view for host/guest ordering.

--- confluence/views/master.php
+++ confluence/views/master.php
@@ -7,19 +7,28 @@ wp_nonce_field($nonce_path, $nonce_key);
 
 ?>
 
-<div class="confluence">
-<p>
+<div class="confluence-metabox">
 
 	<div class="block">
-	<label>This episode's content is <em><span title="Not Safe For Work?">NSFW</span></em>: 
-		<input type="checkbox" name="confluence-nsfw" <?php checked(get_post_meta($object->ID, 'confluence-nsfw', true)); ?> value="<?php esc_attr( get_post_meta($object->ID, 'confluence-nsfw', true) ); ?>" />
-	</label>
+		<p>
+			<label><Strong>Hosts:</strong> <input type="text" class="widefat" name="confluence-hosts-basic" value="<?php esc_attr( get_post_meta($object->ID, 'confluence-hosts-basic', true) ); ?>" /></label>
+		</p>
 	</div>
 
-	<label>Hosts: <input type="text" name="confluence-hosts-basic" /></label>
-	<label>Guests: <input type="text" name="confluence-guests-basic" /></label>
 
-</p>
+	<div class="block">
+		<p>
+			<label><strong>Guests:</strong> <input type="text" class="widefat" name="confluence-guests-basic" value="<?php esc_attr( get_post_meta($object->ID, 'confluence-guests-basic', true) ); ?>" /></label>
+		</p>
+	</div>
+	<div class="block">
+		<p>
+			<label>This episode's content is <em><span title="Not Safe For Work?">NSFW</span></em>: 
+				<input type="checkbox" name="confluence-nsfw" <?php checked(get_post_meta($object->ID, 'confluence-nsfw', true)); ?> value="<?php esc_attr( get_post_meta($object->ID, 'confluence-nsfw', true) ); ?>" />
+			</label>
+		</p>
+	</div>
+
 </div>
 <div>
 <pre>
