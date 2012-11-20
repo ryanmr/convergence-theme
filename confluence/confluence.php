17b9e6ac141debbe2563d3d5003e57a983c7219f
Ryan Rampersad <ryan.rampersad@gmail.com>
2012-11-20 04:17:40 -0600

    Restructured and refactored, preparing people meta box.

--- confluence/confluence.php
+++ confluence/confluence.php
@@ -64,7 +64,8 @@ class Confluence {
 }
 
 class Confluence_People_View {
-	public $nonce_path;
+
+		public $nonce_path;
 	public $nonce_key = 'confluence_nonce';
 
 	public function __construct() {
@@ -86,16 +87,17 @@ class Confluence_People_View {
 
 	public function add_metabox() {
 		add_meta_box(
-			'confluence-people-box',
-			esc_html('People'),
-			array($this, 'display')
+			'confluence-meta-box',
+			esc_html('People Attributes'),
+			array($this, 'display'),
+			'person'
 		);
 	}
 
 	public function display($object, $box) {
 		$nonce_key = $this->nonce_key;
 		$nonce_path = $this->nonce_path;
-		include('views/people.php');
+		include('views/confluence-people.php');
 	}
 
 	public function save($post_id, $post) {
@@ -103,22 +105,13 @@ class Confluence_People_View {
 		if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return $post_id; 
 		if ( !wp_verify_nonce($_POST[$this->nonce_key], $this->nonce_path) ) return $post_id;
 
-		delete_post_meta($post_id, 'confluence-people'); // sadly, but needed
-		// always drop whatever we have right now
-		$people = $_POST['confluence-person']; // because people are singular when not together
-		
-		//var_dump( get_post_meta($post_id, 'confluence-people') ); exit();
+		$this->_nsfw($post_id, $post);
 
-		foreach ($people as $person) {
-			$person = sanitize_title($person);
-			add_post_meta($post_id, 'confluence-people', $person);
-		}
-
-	}	
+	}
 
 }
 
-class Confluence_Master {
+class Confluence_Episode_View {
 
 	public $nonce_path;
 	public $nonce_key = 'confluence_nonce';
@@ -138,25 +131,21 @@ class Confluence_Master {
 	public function metabox_setup() {
 		add_action('add_meta_boxes', array($this, 'add_metabox'));
 		add_action('save_post', array($this, 'save'), 10, 2);
-		add_action('admin_footer', array($this, 'javascript'));
-	}
-
-	public function javascript() {
-		include('views/js/people.js.php');
 	}
 
 	public function add_metabox() {
 		add_meta_box(
 			'confluence-meta-box',
 			esc_html('Confluence'),
-			array($this, 'display')
+			array($this, 'display'),
+			'episode'
 		);
 	}
 
 	public function display($object, $box) {
 		$nonce_key = $this->nonce_key;
 		$nonce_path = $this->nonce_path;
-		include('views/master.php');
+		include('views/confluence-episode.php');
 	}
 
 	public function save($post_id, $post) {
@@ -186,6 +175,67 @@ class Confluence_Master {
 
 }
 
+class Confluence_Episode_People_View {
+	public $nonce_path;
+	public $nonce_key = 'confluence_nonce';
+
+	public function __construct() {
+		$this->nonce_path = basename(__FILE__);
+		if ( is_admin() ) add_action('init', array($this, 'admin_hooks'));
+	}
+
+	public function admin_hooks() {
+		$targets = array('load-post', 'load-post-new');
+		foreach ($targets as $target) {
+			add_action("$target.php", array($this, 'metabox_setup'));
+		}
+	}
+
+	public function metabox_setup() {
+		add_action('add_meta_boxes', array($this, 'add_metabox'));
+		add_action('save_post', array($this, 'save'), 10, 2);
+		add_action('admin_footer', array($this, 'javascript'));
+	}
+
+	public function javascript() {
+		include('views/js/confluence-episode-people.js.php');
+	}
+
+	public function add_metabox() {
+		add_meta_box(
+			'confluence-people-box',
+			esc_html('People'),
+			array($this, 'display'),
+			'episode'
+		);
+	}
+
+	public function display($object, $box) {
+		$nonce_key = $this->nonce_key;
+		$nonce_path = $this->nonce_path;
+		include('views/confluence-episode-people.php');
+	}
+
+	public function save($post_id, $post) {
+
+		if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return $post_id; 
+		if ( !wp_verify_nonce($_POST[$this->nonce_key], $this->nonce_path) ) return $post_id;
+
+		delete_post_meta($post_id, 'confluence-people'); // sadly, but needed
+		// always drop whatever we have right now
+		$people = $_POST['confluence-person']; // because people are singular when not together
+
+		//var_dump( get_post_meta($post_id, 'confluence-people') ); exit();
+
+		foreach ($people as $person) {
+			$person = sanitize_title($person);
+			add_post_meta($post_id, 'confluence-people', $person);
+		}
+
+	}	
+
+}
+
 class Confluence_Interface {
 	public static function get_nsfw() {
 		$meta = get_post_meta( get_the_ID() , "confluence-nsfw", true);
@@ -196,8 +246,8 @@ class_alias("Confluence_Interface", "C");
 
 function confluence_setup() {
 	$confluence = new Confluence();
-	$master = new Confluence_Master();
-	$people_view = new Confluence_People_View();
+	$master = new Confluence_Episode_View();
+	$people_view = new Confluence_Episode_People_View();
 }
 
 confluence_setup();
