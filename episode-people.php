346b104151745f06b4790240c631a5690ce252a0
Ryan Rampersad <ryan.rampersad@gmail.com>
2012-11-22 22:21:53 -0600

    Styled episode-single to present people in the episode cleanly

--- episode-people.php
+++ episode-people.php
@@ -1,7 +1,8 @@
 <?php
 
 	$who_ids = C::get_people_ids();
-	if ( count($who_ids) <= 0 ) return; 
+	if ( count($who_ids) <= 0 ) return;
+	// if there are no people set (or found), skip this setup 
 
 	// we leave the main loop from here temporarily
 	$args = array(
@@ -11,17 +12,13 @@
 	$query = new WP_Query($args);	
 ?>
 	<div class="people">
-			<!--<h3></h3>-->
+			<h3>In This Episode</h3>
 			<div>
 				<?php while ( $query->have_posts() ): $query->the_post(); ?>
 					<div class="person promo">
 						
-						<div class="avatar">
-							<?php echo C::get_person_gravatar(85); ?>
-						</div>
-						<div class="name">
-							<?php the_title(); ?>
-						</div>
+						<div class="avatar"><?php echo C::get_person_gravatar(80); ?></div>
+						<div class="name"><?php the_title(); ?></div>
 						
 					</div>
 				<?php endwhile; ?>
